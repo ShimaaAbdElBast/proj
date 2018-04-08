cbbe440a1b349295ce938146310d2d90b543621c
ShimaaAbdElBast <38190717+ShimaaAbdElBast@users.noreply.github.com>
2018-04-08 22:58:26 +0300

    with Db

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Project_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            this.AddaFacetoTrainingSet = new System.Windows.Forms.Button();
+            this.recognizeTheFace = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.STUid = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.Location = new System.Drawing.Point(1, 0);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(563, 309);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // AddaFacetoTrainingSet
+            // 
+            this.AddaFacetoTrainingSet.Location = new System.Drawing.Point(1, 344);
+            this.AddaFacetoTrainingSet.Name = "AddaFacetoTrainingSet";
+            this.AddaFacetoTrainingSet.Size = new System.Drawing.Size(75, 23);
+            this.AddaFacetoTrainingSet.TabIndex = 3;
+            this.AddaFacetoTrainingSet.Text = "Add a Face";
+            this.AddaFacetoTrainingSet.UseVisualStyleBackColor = true;
+            this.AddaFacetoTrainingSet.Click += new System.EventHandler(this.AddaFaceToTrainingSet_Click);
+            // 
+            // recognizeTheFace
+            // 
+            this.recognizeTheFace.Location = new System.Drawing.Point(512, 384);
+            this.recognizeTheFace.Name = "recognizeTheFace";
+            this.recognizeTheFace.Size = new System.Drawing.Size(75, 23);
+            this.recognizeTheFace.TabIndex = 4;
+            this.recognizeTheFace.Text = "Recognize";
+            this.recognizeTheFace.UseVisualStyleBackColor = true;
+            this.recognizeTheFace.Click += new System.EventHandler(this.recognizeTheFace_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(622, 386);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // STUid
+            // 
+            this.STUid.Location = new System.Drawing.Point(152, 384);
+            this.STUid.Name = "STUid";
+            this.STUid.Size = new System.Drawing.Size(100, 20);
+            this.STUid.TabIndex = 7;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(633, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(202, 309);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 459);
+            this.Controls.Add(this.STUid);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.recognizeTheFace);
+            this.Controls.Add(this.AddaFacetoTrainingSet);
+            this.Controls.Add(this.imgCamUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imgCamUser;
+        private System.Windows.Forms.Button AddaFacetoTrainingSet;
+        private System.Windows.Forms.Button recognizeTheFace;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox STUid;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.IO;
+using Finisar.SQLite; // db
+
+namespace Project_1_1
+{
+    public partial class Form1 : Form
+    {
+        string LoadFaces;
+        Image<Gray, byte> TrainedFace = null, unknownFace = null;
+        private VideoCapture _capture;
+        private CascadeClassifier _cascadeClassifier;
+        private CascadeClassifier trainCascadeClassifier;
+        List<Image<Gray, byte>> trainingImagesHolder = new List<Image<Gray, byte>>();
+        List<string> labelsHolder = new List<string>();
+        int[] integerLablesHolder;
+        int numberOfLables, CountTrainedFaces;
+        MEigenRecognizer myRecognizer;
+        SQLiteConnection sqlite_conn;
+        SQLiteCommand sqlite_cmd;
+        SQLiteDataReader sqlite_datareader;
+
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+            //intialize the eigen recognizer to be used later 
+            myRecognizer = new MEigenRecognizer("E:/Project_1_1/bin/Debug/holdRecFilePath/thephotos.yml");
+
+            //Now get the saved imagesin the TrainedFaces Folder
+            try
+            {
+                //First get the lables of the images in the training set where the lables are the names of users
+                //string getLabels = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                // numberOfLables = Convert.ToInt16(Labels[0]);
+                List<string> image = new List<string> ();
+                CountTrainedFaces = image.Count;
+                sqlite_conn = new SQLiteConnection("Data Source=E:/Project_1_1/Project.db ; Version =3;");
+
+                sqlite_conn.Open();
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                //create command
+                sqlite_cmd.CommandText = "select image_path from studentsFaces ;";
+                //datareader to read from db
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+                while (sqlite_datareader.Read())
+                {
+                    image.Add(sqlite_datareader.GetString(0));
+                }
+                sqlite_cmd.ExecuteNonQuery();
+                //close connections
+                sqlite_conn.Close();
+                sqlite_datareader.Close();
+
+                string[] Labels = new string[image.Count];
+                for (int i = 0; i < image.Count; i++)
+                {
+                    Labels[i] = image[i];
+                }
+
+                // sqlite_conn.Close();
+                //Now Load the images from the TrainedFaces Folder to our List that the images to be used in the code later
+                //And the labels to our List that holds the lables to be used later .
+                for (int i = 0; i <= image.Count ; i++)
+                {
+                    LoadFaces = "face" + i + ".bmp";
+                    trainingImagesHolder.Add(new Image<Gray, byte>(image[i]));
+                    labelsHolder.Add(Labels[i]);
+                }
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.ToString());
+                MessageBox.Show("Nothing in binary database, please add at least a face"
+                   + " (Simply train the prototype with the Add Face Button).", "Triained faces load",
+                   MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Run();
+
+
+        }
+
+        private void Run()
+        {
+            try
+            {
+                _capture = new VideoCapture();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            Application.Idle += ProcessFrame;
+
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+
+            _cascadeClassifier = new CascadeClassifier("C:/Emgu/emgucv-windesktop 3.2.0.2682/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+            using (var imageFrame = _capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, byte>();
+                    var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                    foreach (var face in faces)
+                    {
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                    }
+                }
+
+                imgCamUser.Image = imageFrame;
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Test_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < labelsHolder.ToArray().Length; i++)
+            {
+                string[] arrayLablesHolder = labelsHolder.ToArray();
+                //  integerLablesHolder[i] = Int32.Parse(arrayLablesHolder[i]);
+                Console.WriteLine(arrayLablesHolder[i]);
+            }
+        }
+
+        private void recognizeTheFace_Click(object sender, EventArgs e)
+        {
+            trainCascadeClassifier = new CascadeClassifier("C:/Emgu/emgucv-windesktop 3.2.0.2682/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+            using (var currentFrame = _capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+
+                if (currentFrame != null)
+                {
+                    var grayframe = currentFrame.Convert<Gray, byte>();
+                    var faces = trainCascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                    foreach (var face in faces)
+                    {
+                        grayframe.Draw(face, new Gray(1.523), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                    }
+                    unknownFace = grayframe;
+                }
+            }
+            int lable = myRecognizer.RecognizeUser(unknownFace);
+            Console.Write(lable);
+            MessageBox.Show(""
+                  + lable, "Detected Face",
+                  MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        private void AddaFaceToTrainingSet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                trainCascadeClassifier = new CascadeClassifier("C:/Emgu/emgucv-windesktop 3.2.0.2682/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+                using (var currentFrame = _capture.QueryFrame().ToImage<Bgr, Byte>())
+                {
+
+                    if (currentFrame != null)
+                    {
+                        var grayframe = currentFrame.Convert<Gray, byte>();
+                        var faces = trainCascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                        foreach (var face in faces)
+                        {
+                            grayframe.Draw(face, new Gray(1.523), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                        }
+                        grayframe = grayframe.Resize(100, 100, Inter.Cubic);
+                        TrainedFace = grayframe;
+                    }
+
+                }
+                trainingImagesHolder.Add(TrainedFace);
+                labelsHolder.Add(textBox1.Text);
+
+                //Show face added in gray scale
+                imageBox1.Image = TrainedFace;
+
+                //Write the number of triained faces in a file text for further load
+                //   File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImagesHolder.ToArray().Length.ToString() + "%");
+
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_conn.Open();
+                MessageBox.Show("insert into studentsFaces values (" + int.Parse(STUid.Text) + ",'" + Application.StartupPath + "\\TrainedFaces" + LoadFaces + "')");
+                sqlite_cmd.CommandText = "insert into studentsFaces values (" + int.Parse(STUid.Text) +",'"+ Application.StartupPath + "\\TrainedFaces" + LoadFaces +"')";
+
+                sqlite_conn.Close();
+
+                //Write the labels of triained faces in a file text for further load
+               /* for (int i = 1; i < trainingImagesHolder.ToArray().Length + 1; i++)
+                {
+                    trainingImagesHolder.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labelsHolder.ToArray()[i - 1] + "%");
+                }*/
+
+                MessageBox.Show(textBox1.Text + "´s face detected and added :)", "Training OK",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Enable the face detection first", "Training Fail",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            //----------------------------------------------------------------------------------------------------------------
+            //Now after we added the new images and lables to our training set we will train our eigen recognizer
+            integerLablesHolder = new int[labelsHolder.ToArray().Length];
+            for (int i = 0; i < labelsHolder.ToArray().Length; i++)
+            {
+                string[] arrayLablesHolder = labelsHolder.ToArray();
+                // integerLablesHolder[i]= Convert.ToInt32(arrayLablesHolder[i]);
+                Int32.TryParse(arrayLablesHolder[i], out integerLablesHolder[i]);
+
+            }
+            myRecognizer.TrainRecognizer(trainingImagesHolder.ToArray(), integerLablesHolder);
+        }
+
+
+    }
+}
